eaf6a5fb64cbee070f16d70ee6f9f03803cea298
ERUKULLAANUDEEP <39010804+ERUKULLAANUDEEP@users.noreply.github.com>
2026-04-18 00:48:06 +0530

    app-wired

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Stage 1: Build
+FROM node:18 AS builder
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+RUN npm run build   # 🔥 creates dist/
+
+# Stage 2: Production
+FROM node:18-slim
+
+WORKDIR /app
+
+COPY --from=builder /app/dist ./dist
+COPY --from=builder /app/package*.json ./
+
+RUN npm install --only=production
+
+EXPOSE 3000
+
+CMD ["node", "dist/index.js"]
---
+++ index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+});
+
+app.get("/", (req, res) => {
+  res.send("TypeScript Docker 🚀");
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching users");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
